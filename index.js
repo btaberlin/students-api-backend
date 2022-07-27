ae1e687e62c4bab00664c80a2e6e31cf670f0047
Reyan P <softwareharddata@gmail.com>
2022-07-27 13:20:40 +0200

    PORT

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const { response } = require('express')
 
 const express = require('express')
 const app = express()
-const port = process.env.port || 3001
+const port = process.env.Port || 3001
 app.use(express.json())
 
 
